5843e5a5fe63b51e88fdaca05af5d64d6d475f8b
pham-anh <pham-anh@users.noreply.github.com>
2016-10-05 23:28:31 +0700

    Copy Base PDO config in core/config/db.php

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -7,4 +7,26 @@
 
 return array(
 
+	/**
+	 * Base PDO config
+	 */
+	'default' => array(
+		'type'        => 'pdo',
+		'connection'  => array(
+			'dsn'        => '',
+			'hostname'   => '',
+			'username'   => null,
+			'password'   => null,
+			'database'   => '',
+			'persistent' => false,
+			'compress'   => false,
+		),
+		'identifier'   => '`',
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'collation'    => false,
+		'enable_cache' => true,
+		'profiling'    => false,
+		'readonly'     => false,
+	),
 );
