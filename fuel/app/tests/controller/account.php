f667162b73042d77d36e5959bf2172209a9d18fa
pham-anh <pham-anh@users.noreply.github.com>
2016-11-08 00:58:08 +0700

    Add test case test_acction_create_returns_presenter

---
+++ fuel/app/tests/controller/account.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @group App
+ */
+class Test_Controller_Account extends TestCase
+{
+
+	public function test_acction_create_returns_presenter()
+	{
+		$response = Request::forge('account/create')->execute()->response();
+
+		$presenter = get_class($response->body());
+		$this->assertEquals('Presenter_Account_Create', $presenter);
+	}
+
+}
