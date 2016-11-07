35e27483fa802e5f58dcb862024f625152effd5d
pham-anh <pham-anh@users.noreply.github.com>
2016-11-08 00:29:49 +0700

    Modify layout of create form

--- fuel/app/views/account/_form.php
+++ fuel/app/views/account/_form.php
@@ -1,32 +1,37 @@
-<?php echo Form::open(array("class"=>"form-horizontal")); ?>
 
-	<fieldset>
-		<div class="form-group">
-			<?php echo Form::label('Account name', 'account_name', array('class'=>'control-label')); ?>
+<?php echo Form::open(array("class" => "center-block")); ?>
 
-				<?php echo Form::input('account_name', Input::post('account_name', isset($account) ? $account->account_name : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Account name')); ?>
+<fieldset>
+	<div class="form-group col-sm-6">
+		<?php echo Form::label('Account name', 'account_name', array('class' => 'control-label')); ?>
+		
+			<?php echo Form::input('account_name', Input::post('account_name', isset($account) ? $account->account_name : ''), array('class' => 'form-control ', 'placeholder' => 'Account name')); ?>
+		
+	</div>
+	<div class="form-group col-sm-6">
+		<?php echo Form::label('Account no', 'account_no', array('class' => 'control-label col-sm-4')); ?>
+		
+			<?php echo Form::input('account_no', Input::post('account_no', isset($account) ? $account->account_no : ''), array('class' => 'form-control col-sm-8', 'placeholder' => 'Account no')); ?>
 
-		</div>
-		<div class="form-group">
-			<?php echo Form::label('Account no', 'account_no', array('class'=>'control-label')); ?>
+	</div>
+	<div class="form-group col-sm-6">
+		<?php echo Form::label('Bank', 'bank', array('class' => 'control-label text-overflow')); ?>
 
-				<?php echo Form::input('account_no', Input::post('account_no', isset($account) ? $account->account_no : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Account no')); ?>
-
-		</div>
-		<div class="form-group">
-			<?php echo Form::label('Bank', 'bank', array('class'=>'control-label')); ?>
-
-				<?php echo Form::input('bank', Input::post('bank', isset($account) ? $account->bank : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Bank')); ?>
-
-		</div>
-		<div class="form-group">
-			<?php echo Form::label('Province', 'province', array('class'=>'control-label')); ?>
-
-				<?php echo Form::input('province', Input::post('province', isset($account) ? $account->province : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Province')); ?>
+			<?php echo Form::input('bank', Input::post('bank', isset($account) ? $account->bank : ''), array('class' => 'form-control', 'placeholder' => 'Bank', '')); ?>
 
 		</div>
-		<div class="form-group">
+	<div class="form-group col-sm-6">
+		<?php echo Form::label('Province', 'province', array('class'=>'control-label')); ?>
+
+			<?php echo Form::input('province', Input::post('province', isset($account) ? $account->province : ''), array('class' => 'form-control', 'placeholder' => 'Province')); ?>
+
+		</div>
+		<div class="form-group col-sm-6 text-right">
 			<label class='control-label'>&nbsp;</label>
-			<?php echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); ?>		</div>
-	</fieldset>
+			<?php echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); ?>
+		</div>
+		<div class="form-group col-sm-3">
+			<?php echo Html::anchor('account', 'Cancel', array('class' => 'btn btn-default')); ?>
+		</div>
+</fieldset>
 <?php echo Form::close(); ?>
