205bb23e044ee29cbdc96e97b21d8982012094c4
pham-anh <pham-anh@users.noreply.github.com>
2016-11-08 00:41:00 +0700

    Remove commentted out code

--- fuel/app/views/account/create.php
+++ fuel/app/views/account/create.php
@@ -1,7 +1,2 @@
-<!--<h2>New Account</h2>-->
-<!--<br>-->
 
 <?php echo render('account/_form'); ?>
-
-
-<!--<p><?php // echo Html::anchor('account', 'Back', array('class' => 'btn btn-default'));  ?></p>-->
