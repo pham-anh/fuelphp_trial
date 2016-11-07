11fecfddbb09d910a8fd81162f0666979e8228fc
pham-anh <pham-anh@users.noreply.github.com>
2016-11-08 00:34:25 +0700

    Remove subtitle and back button from view

--- fuel/app/views/account/create.php
+++ fuel/app/views/account/create.php
@@ -1,7 +1,7 @@
-<h2>New Account</h2>
-<br>
+<!--<h2>New Account</h2>-->
+<!--<br>-->
 
 <?php echo render('account/_form'); ?>
 
 
-<p><?php echo Html::anchor('account', 'Back'); ?></p>
+<!--<p><?php // echo Html::anchor('account', 'Back', array('class' => 'btn btn-default'));  ?></p>-->
