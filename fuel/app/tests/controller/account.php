3e4ecaba3072847975d4a19c2c8594630b927078
pham-anh <pham-anh@users.noreply.github.com>
2016-11-27 18:48:08 +0700

    #31 Review test cases for controler create Remove test case with Goutte Rewrite wrong test case to check cancel link Add test data cleaning up

--- fuel/app/tests/controller/account.php
+++ fuel/app/tests/controller/account.php
@@ -14,40 +14,8 @@ class Test_Controller_Account extends TestCase
 		$this->assertEquals('Presenter_Account_Create', $presenter);
 	}
 
-	public function test_create_with_get_returns_empty_form_test_with_goutte()
-	{
-		$client = new Goutte\Client();
-		$response = $client->request('GET', 'http://localhost/payment_system/public/account/create');
-
-		//has a form
-		$this->assertCount(1, $response->filter('form'));
-
-		//check the form empty
-		$this->assertEquals('', $response->filter('form input[name=account_name]')->attr('value'));
-		$this->assertEquals('', $response->filter('form input[name=account_no]')->attr('value'));
-		$this->assertEquals('', $response->filter('form input[name=bank]')->attr('value'));
-		$this->assertEquals('', $response->filter('form input[name=province]')->attr('value'));
-
-		//has 4 input fields
-		$this->assertCount(4, $response->filter('form input.form-control'));
-
-		//has 4 placeholders
-		$this->assertCount(4, $response->filter('form input[placeholder]'));
-
-		//has 1 submit button
-		$this->assertCount(1, $response->filter('form input[name=submit]'));
-
-		//cancel button is linked to account List
-		$cancel_link = $response->selectLink('Cancel')->link();
-		$cancel_uri = $cancel_link->getUri();
-		$this->assertEquals('http://localhost/payment_system/public/account/index', $cancel_uri);
-	}
-
 	public function test_create_with_get_returns_empty_form()
 	{
-		//$client = new Goutte\Client();
-		//$response = $client->request('GET', 'http://localhost/payment_system/public/account/create');
-
 		$url = 'http://localhost/payment_system/public/account/create';
 		$curl = Request::forge($url, 'curl');
 		$curl->set_method('GET');
@@ -71,9 +39,9 @@ class Test_Controller_Account extends TestCase
 		$placeholders = preg_match_all('/(<input)*(placeholder=)/', $res_info['response']);
 		$this->assertEquals(4, $placeholders);
 
-		//cancel button is linked to account index
-		$account_index_url = 'http://localhost/payment_system/public/account/index';
-		$cancel_link = preg_match_all('/(<a)*(href="$account_index_url")*(Cancel)/', $res_info['response']);
+		//has a link to account index (cancel button)
+		$expected_link = '/(localhost\/payment_system\/public\/account\/index)/';
+		$cancel_link = preg_match_all($expected_link, $res_info['response']);
 		$this->assertEquals(1, $cancel_link);
 	}
 
@@ -83,19 +51,22 @@ class Test_Controller_Account extends TestCase
 		$curl = Request::forge($url, 'curl');
 		$curl->set_method('POST');
 
+		$account_no = rand(1000000000, 9999999999);
 		$params = array(
 			'account_name' => 'ASDFGHJK',
-			'account_no' => '012389rd687',
+			'account_no' => "$account_no",
 			'bank' => 'QWERT BANK',
 			'province' => 'HO CHI MINH');
+
 		$curl->set_params(array('form-data' => $params));
 		$curl->execute();
+
 		$res_info = $curl->response_info();
-
 		$redirect = 'http://localhost/payment_system/public/account/index';
-
-
 		$this->assertEquals($res_info['url'], $redirect);
+
+		//clean up test data
+		DB::delete('accounts')->where('account_no', '=', $account_no)->execute();
 	}
 
 }
