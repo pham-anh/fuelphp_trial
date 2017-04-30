034ca5437aff91b002c467ee892c4373dad7dc10
pham-anh <pham-anh@users.noreply.github.com>
2017-04-30 19:53:42 +0700

    #1 Enable profiling for development mode only & show query in profiling panel

---
+++ fuel/app/config/development/config.php
@@ -0,0 +1,6 @@
+<?php
+
+// Over write the profiling setting in fuel/app/config when using in development mode
+return array(
+	'profiling' => true,
+);
--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,10 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
-			'username'   => 'root',
-			'password'   => 'root',
+			'dsn'        => 'mysql:host=foohost;dbname=foodb',
+			'username'   => 'foouser',
+			'password'   => 'foopasswd',
 		),
+		'profiling' => true, //add here to see query
 	),
 );
